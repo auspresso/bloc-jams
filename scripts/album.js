f197afb38e550501b08cce01837f248b2c0d35f7
Austin Marticorena <auspresso@gmail.com>
2017-03-14 11:46:48 -0600

    assignment-27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -109,15 +109,31 @@ var currentlyPlayingSong = null;
          }
      });
      
-     var findParentByClassName = function(element, targetClass) {
+var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+        currentParent = currentParent.parentElement;
         }
         return currentParent;
     }
 };
+     
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+        currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+        else if (typeof findParentByClassName == 'function') { 
+        findParentByClassName(); 
+        }else{
+        console.log('No parent found with that class name');
+}
+};
+     
 var getSongItem = function(element) {
     switch (element.className) {
         case 'album-song-button':
